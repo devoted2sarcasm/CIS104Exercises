5b1b9459a851713876b56296845755589c229b74
devoted2sarcasm <97539267+devoted2sarcasm@users.noreply.github.com>
2022-03-01 20:14:34 -0500

    my code + copies of Jade's

---
+++ week7/ex73.py
@@ -0,0 +1,15 @@
+fname = input('Input file name: ')
+if fname == 'na na boo boo':
+    print("NA NA BOO BOO TO YOU - You have been punk'd")
+else:
+    try:
+        fhand = open(fname)
+    except:
+        print('File could not be opened:', fname)
+        quit()
+    count = 0
+    for line in fhand:
+        if line.startswith('Subject:'):
+            count = count + 1
+    print('There were', count, 'subject lines in', fname)
+    
--- week7/exercise1.py
+++ week7/exercise1.py
@@ -3,4 +3,3 @@ fhand = open('mbox-short.txt')
 for line in fhand:
     line = line.rstrip()
     print(line.upper())
-    

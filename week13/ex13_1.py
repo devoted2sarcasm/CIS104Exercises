ef1196c4f1bdfa2669a7b49e8d2edd2f3227b4e2
devoted2sarcasm <97539267+devoted2sarcasm@users.noreply.github.com>
2022-04-19 21:31:44 -0400

    Update ex13_1.py

--- week13/ex13_1.py
+++ week13/ex13_1.py
@@ -10,41 +10,24 @@ api_key = False
 serviceurl = 'http://py4e-data.dr-chuck.net/xml?'
 
 
-#if api_key is False:
-#    api_key = 42
-#    serviceurl = 'http://py4e-data.dr-chuck.net/xml?'
-#else :
-#    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
-
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-total = 0
+
 while True:
     address = input('Enter url: ')
     if len(address) < 1: break
 
-#    parms = dict()
-#    parms['address'] = address
-#    if api_key is not False: parms['key'] = api_key
-#    url = serviceurl + urllib.parse.urlencode(parms)
-#    print('Retrieving', url)
     uh = urllib.request.urlopen(address, context=ctx)
 
     data = uh.read()
     print('Retrieved', len(data), 'characters')
-    data2 = data.decode()
-    print(data2)
-    tree = ET.fromstring(data2)
-#    print(tree)
+    tree = ET.fromstring(data)
     results = tree.findall('comments/comment/count')
-    print(results)
+    counts = tree.findall('.//count')
+    total = 0
+    for count in counts:
+        total = total + int(count.text)
 
-#    for num in results:
-
-#    comment_count = results.find('commentinfo').find('comments').findall('comment').find('count').text
-#    lng = results[0].find('geometry').find('location').find('lng').text
-#    location = results[0].find('formatted_address').text
-#    total = total + comment_count
     print('Comments: ', total)
